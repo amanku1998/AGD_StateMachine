3b351f7571dfeb06cc99e36557ef90c20cc37219
Aman Kumar <aman.kumar01747@gmail.com>
2025-08-10 20:54:57 +0530

    -Create a state machine

---
+++ Assets/Scripts/Enemy/OnePunchMan/OnePunchManStateMachine.cs
@@ -0,0 +1,53 @@
+using StatePattern.Enemy;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnePunchManStateMachine 
+{
+    // Reference to the owner of the state machine
+    private OnePunchManController Owner;
+
+    // Reference to the current state
+    private IState currentState;
+
+    // Dictionary to store available states, mapped to enum values
+    protected Dictionary<OnePunchManStates, IState> States = new Dictionary<OnePunchManStates, IState>();
+
+    public OnePunchManStateMachine(OnePunchManController Owner)
+    {
+        this.Owner = Owner;
+        CreateStates();
+        SetOwner();
+    }
+
+    // Create and initialize the states
+    private void CreateStates()
+    {
+        States.Add(OnePunchManStates.IDLE, new IdleState(this));
+        States.Add(OnePunchManStates.ROTATING, new RotatingState(this));
+        States.Add(OnePunchManStates.SHOOTING, new ShootingState(this));
+    }
+
+    // Set the owner for each state
+    private void SetOwner()
+    {
+        foreach (IState state in States.Values)
+        {
+            state.Owner = Owner;
+        }
+    }
+
+    // Update the current state (if it exists)
+    public void Update() => currentState?.Update();
+
+    // Change the current state to a new state
+    protected void ChangeState(IState newState)
+    {
+        currentState?.OnStateExit(); // Exit the current state (if it exists)
+        currentState = newState; // Set the new state
+        currentState?.OnStateEnter(); // Enter the new state
+    }
+
+    // Change the current state to a new state by providing a state enum
+    public void ChangeState(OnePunchManStates newState) => ChangeState(States[newState]);
+}
